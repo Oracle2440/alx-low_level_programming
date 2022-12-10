b6734ed98551c7f1f656bbfba2b8b144b89f9c55
oracle2440 <trinityivory@gmail.com>
2022-12-10 13:47:50 -0800

    assembly_code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
 /**
  * main - Entry Point
  *
- * Return Always 1 (Success)
+ * Return Always 0 (success)
  */
 
 int main(void)
-
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-
+	return (0);
 }
