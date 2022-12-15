9a7ad7eaecd660db54002beb18ebfac94b045c6e
oracle2440 <trinityivory@gmail.com>
2022-12-15 11:20:22 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * more_numbers - nmbr fct
- *
- * Return: no return
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
-int i; j;
+int i, j;
 for (i = 1; i <= 10; i++)
 {
 for (j = 0; j < 15; j++)
