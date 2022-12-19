4e239928ebdd8cea5ccffd4d60c88eb1f36e7244
oracle2440 <trinityivory@gmail.com>
2022-12-19 09:32:53 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,25 +6,27 @@
  */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-while (*(s + count) != '\0')
-{
-if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-break;
-if (*(s + count) == '-')
-pn *= -1;
-if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-{
-if (size > 0)
-m *= 10;
-size++;
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while
+			(*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
-count++;
-}
-for (i = count - size; i < count; i++)
-{
-oi = oi + ((*(s + i) - 48) * m);
-m /= 10;
-}
-return (oi * pn);
-}
+
